764d14c283e5ab0723e82e0ab3459605ad6d99f2
Yash Ganeshgudi <153029336+yashGaneshgudi@users.noreply.github.com>
2024-05-06 14:49:37 +0100

    Create NormalModel.py

---
+++ NormalModel.py
@@ -0,0 +1,17 @@
+import random
+
+results=[]
+
+for i in range (100000):
+    heads=0
+    print(i)
+    for i in range(10):
+        coin = random.randint(0, 1)
+        if coin == 1:
+            heads += 1
+    results.append(heads)
+    print(heads)
+
+import matplotlib.pyplot as plt
+plt.hist(results)
+plt.show()
